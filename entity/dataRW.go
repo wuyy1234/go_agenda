c899ed5b0ba7e55b26c85da85a862056a593e899
wyy <wyy@centos7.localdomain>
2018-10-30 23:29:56 +0800

    init by wyy

--- entity/dataRW.go
+++ entity/dataRW.go
@@ -20,8 +20,9 @@ type User struct {
 
 /*Time 时间*/
 type Time struct {
-	Hour   int
-	Minute int
+	timeID int //时间段，上午两段下午两段，编号1，2，3，4
+	day    int
+	month  int
 }
 
 /*Meeting 会议*/
@@ -29,9 +30,11 @@ type Meeting struct {
 	Title        string
 	Sponsor      string   //发起者
 	Paticipators []string //参与者用户名
-	StartTime    Time
-	EndTime      Time
+	MeetingTime  []Time   //可以包括多个时间段
 }
+
+/*CurrentUserName 当前登陆用户*/
+var CurrentUserName string
 
 /*READUSERS  读取文件*/
 func READUSERS() (user []User) {
@@ -82,31 +85,6 @@ func WRITEMEETINGS(meetings []Meeting) {
 	err = ioutil.WriteFile(dir+"/entity/Meetings.txt", b, 0777)
 	checkerr(err)
 	log.Println("WRITEMEETINGS success")
-}
-
-/*READCURRENTUSER 读取当前登陆的用户信息*/
-func READCURRENTUSER() (user User) {
-	dir, err := os.Getwd()
-	checkerr(err)
-	b, err := ioutil.ReadFile(dir + "/entity/CurrentUser.txt")
-	checkerr(err)
-	//json转变为对象
-	var users User
-	json.Unmarshal(b, &users)
-	log.Println("READCURRENTUSER success")
-	return users
-}
-
-/*WRITECURRENTUSER 编辑当前登陆的用户信息*/
-func WRITECURRENTUSER(users User) {
-	dir, err := os.Getwd()
-	checkerr(err)
-	data, err := json.Marshal(users)
-	checkerr(err)
-	b := []byte(data)
-	err = ioutil.WriteFile(dir+"/entity/CurrentUser.txt", b, 0777)
-	checkerr(err)
-	log.Println("WRITECURRENTUSER success")
 }
 
 func checkerr(err error) {
